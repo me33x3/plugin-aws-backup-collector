3bc8adb1990be8a0317372c620fcda9091054823
me33x3 <me33x3@gmail.com>
2024-08-27 23:03:48 +0900

    Backup Job 수집

--- src/spaceone/inventory/connector/aws_backup_connector/connector.py
+++ src/spaceone/inventory/connector/aws_backup_connector/connector.py
@@ -5,8 +5,8 @@ from spaceone.core import utils
 from datetime import datetime, timedelta
 from botocore.exceptions import ClientError
 
-from spaceone.inventory.connector.aws_backup_connector.schema.data import BackupPlan, Rule
-from spaceone.inventory.connector.aws_backup_connector.schema.resource import BackupPlanResource, BackupPlanResponse
+from spaceone.inventory.connector.aws_backup_connector.schema.data import Backup, Rule
+from spaceone.inventory.connector.aws_backup_connector.schema.resource import BackupJobResource, BackupJobResponse
 from spaceone.inventory.connector.aws_backup_connector.schema.service_type import CLOUD_SERVICE_TYPES
 from spaceone.inventory.libs.connector import SchematicAWSConnector
 from spaceone.inventory.libs.schema.resource import ReferenceModel
@@ -17,13 +17,13 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class BackupConnector(SchematicAWSConnector):
-    response_schema = BackupPlanResponse
+    response_schema = BackupJobResponse
     service_name = 'backup'
     cloud_service_group = 'Backup'
-    cloud_service_type = 'BackupPlan'
+    cloud_service_type = 'BackupJob'
     cloud_service_types = CLOUD_SERVICE_TYPES
 
-    def get_resources(self) -> List[BackupPlanResource]:
+    def get_resources(self) -> List[BackupJobResource]:
         _LOGGER.debug(f"[get_resources][account_id: {self.account_id}] START: Backup")
         resources = []
         start_time = time.time()
@@ -34,7 +34,7 @@ class BackupConnector(SchematicAWSConnector):
             try:
                 self.reset_region(region_name)
 
-                for data in self.request_data(region_name):
+                for data in self.request_data():
                     if getattr(data, 'resource_type', None) and data.resource_type == 'inventory.ErrorResource':
                         # Error Resource
                         resources.append(data)
@@ -47,7 +47,7 @@ class BackupConnector(SchematicAWSConnector):
                             'reference': ReferenceModel(data.reference())
                         }
 
-                        resources.append(self.response_schema({'resource': BackupPlanResource(backup_plan_resource)}))
+                        resources.append(self.response_schema({'resource': BackupJobResource(backup_plan_resource)}))
             except Exception as e:
                 error_resource_response = self.generate_error(region_name, '', e)
                 resources.append(error_resource_response)
@@ -55,32 +55,25 @@ class BackupConnector(SchematicAWSConnector):
         _LOGGER.debug(f'[get_resources][account_id: {self.account_id}] FINISHED: Backup ({time.time() - start_time} sec)')
         return resources
 
-    def request_data(self, region_name) -> List[BackupPlan]:
-        # cloudtrail_resource_type = 'AWS::Backup::BackupPlan'
-        response = self.client.list_backup_plans()
+    def request_data(self) -> List[Backup]:
+        now = datetime.utcnow()
+        start_time = now - timedelta(hours=24)
 
-        for raw in response.get('BackupPlansList', []):
+        response = self.client.list_backup_jobs(ByCreatedAfter=start_time, ByCreatedBefore=now)
+
+        for raw in response.get('BackupJobs', []):
             try:
-                backup_plan_id = raw.get('BackupPlanId')
+                backup_job_id = raw.get('BackupJobId')
 
                 raw.update({
-                    'backup_plan_id': backup_plan_id,
+                    # 'backup_plan_id': backup_job_id,
                     # 'cloudtrail': self.set_cloudtrail(region_name, cloudtrail_resource_type, backup_plan_id),
                 })
 
-                self.get_backup_plan(backup_plan_id)
+                # self.get_backup_plan(backup_job_id)
 
-                yield BackupPlan(raw, strict=False)
+                yield Backup(raw, strict=False)
             except Exception as e:
                 resource_id = raw.get('BackupPlanName', '')
                 error_resource_response = self.generate_error('global', resource_id, e)
                 yield error_resource_response
-
-    def get_backup_plan(self, backup_plan_id):
-        try:
-            response = self.client.get_backup_plan(BackupPlanId=backup_plan_id)
-
-            return None
-        except Exception as e:
-            _LOGGER.error(f'[Backup {backup_plan_id}: Get Backup Plan] {e}')
-            return None
--- src/spaceone/inventory/connector/aws_backup_connector/schema/data.py
+++ src/spaceone/inventory/connector/aws_backup_connector/schema/data.py
@@ -14,10 +14,12 @@ class Rule(Model):
     name = StringType(default="")
 
 
-class BackupPlan(AWSCloudService):
-    arn = StringType(deserialize_from="BackupPlanArn")
-    name = StringType(deserialize_from="BackupPlanName")
-    backup_plan_id = StringType(default="")
+class Backup(AWSCloudService):
+    arn = StringType(deserialize_from="RecoveryPointArn")
+    name = StringType(deserialize_from="ResourceName")
+    state = StringType(deserialize_from="State")
+    status_message = StringType(deserialize_from="StatusMessage")
+    resource_type = StringType(deserialize_from="ResourceType")
 
     def reference(self):
         return {
